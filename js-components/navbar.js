a912b85c97860313255b71feb1606590e266c865
almitte <almista01@gmail.com>
2023-09-06 08:53:08 +0200

    ccc

--- js-components/navbar.js
+++ js-components/navbar.js
@@ -25,6 +25,9 @@ class Navbar extends HTMLElement{
                     <a href="../dashboards/rollout_planung_2022.html">Rollout Planung 2022</a>
                     <a href="../dashboards/rollout_planung_2023.html">Rollout Planung 2023</a>
                     <a href="../dashboards/rollout_planung_2024.html">Rollout Planung 2024</a>
+                    <hr>
+                    <a href="../dashboards/servermigration_2023_r350.html">Servermigration 2023 R350</a>
+                    <a href="../dashboards/servermigration_2024_r350.html">Servermigration 2024 R350</a>
                 </div>
             </div>
             <div class="dropdown">
@@ -48,6 +51,7 @@ class Navbar extends HTMLElement{
                     <a href="../dashboards/rollout_planung_2024.html">Rollout Planung 2024</a>
                     <hr>
                     <a href="../dashboards/servermigration_2023_r350.html">Servermigration 2023 R350</a>
+                    <a href="../dashboards/servermigration_2024_r350.html">Servermigration 2024 R350</a>
                 </div>
             </div>
             <div class="dropdown">
