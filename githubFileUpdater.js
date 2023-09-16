f297c125009ba4d8c2c78e6d9e646e5f05e65a3d
almitte <almista01@gmail.com>
2023-09-16 22:32:16 +0200

    CountUP

--- githubFileUpdater.js
+++ githubFileUpdater.js
@@ -1,62 +1,81 @@
 // githubFileUpdater.js
 
-function updateGitHubFile(githubToken, repoOwner, repoName, filePath) {
-    const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`;
-    const headers = new Headers({
-      'Authorization': `token ${githubToken}`,
-      'Content-Type': 'application/json',
-    });
-  
-    // Function to fetch the latest content
-    function fetchLatestContent() {
-      return fetch(apiUrl, { headers })
-        .then(response => response.json());
-    }
-  
-    // Function to update the file
-    function performFileUpdate(currentData) {
-      const currentContent = atob(currentData.content);
-      let currentNumber = parseInt(currentContent, 10);
-      if (isNaN(currentNumber)) {
-        currentNumber = 0;
-      }
-      const updatedNumber = currentNumber + 1;
-      const updatedContent = updatedNumber.toString();
-  
-      const updateData = {
-        message: 'Update file content',
-        content: btoa(updatedContent),
-        sha: currentData.sha,
-      };
-  
-      const updateUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`;
-  
-      return fetch(updateUrl, {
-        method: 'PUT',
-        headers,
-        body: JSON.stringify(updateData),
-      });
-    }
-  
-    // Fetch the latest content and perform the update
-    fetchLatestContent()
-      .then(currentData => {
-        return performFileUpdate(currentData);
-      })
-      .then(response => {
-        if (response.status === 200) {
-          console.log('File updated successfully.');
-        } else if (response.status === 409) {
-          // Handle conflict by retrying the update
-          console.warn('Conflict encountered. Retrying...');
-          return fetchLatestContent()
-            .then(currentData => performFileUpdate(currentData));
-        } else {
-          console.error('Error updating file:', response.statusText);
-        }
-      })
-      .catch(error => {
-        console.error('An error occurred:', error);
-      });
+function countUp(){
+
+const githubToken = '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';
+const repoOwner = 'almitte';
+const repoName = 'almitte.github.io';
+const filePath = 'Counter.txt';
+
+
+// Create a URL for the GitHub API to get the current content of the file
+const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`;
+
+// Headers for the API request, including authentication
+const headers = new Headers({
+  'Authorization': `token ${githubToken}`,
+  'Content-Type': 'application/json',
+});
+
+// Function to handle error responses
+function handleErrorResponse(response) {
+  if (response.status === 401) {
+    console.error('Unauthorized: You may need to check your GitHub token.', response);
+  } else if (response.status === 409) {
+    console.error('Conflict: There was a conflict while updating the file.', response);
+  } else {
+    console.error('Error updating file:', response.status, response.statusText);
   }
+}
+
+// Fetch the current file content
+fetch(apiUrl, { headers })
+  .then(response => {
+    if (response.status === 200) {
+      return response.json();
+    } else {
+      handleErrorResponse(response);
+      throw new Error('Failed to fetch file content');
+    }
+  })
+  .then(data => {
+    // Decode the content from Base64
+    const currentContent = atob(data.content);
+
+    // Convert the current content to a number and increment it by one
+    let currentNumber = parseInt(currentContent, 10);
+    if (isNaN(currentNumber)) {
+      currentNumber = 0; // If the content is not a number, start with 0
+    }
+    const updatedNumber = currentNumber + 1;
+
+    // Create a new object with the updated content
+    const updatedContent = updatedNumber.toString();
+
+    const updateData = {
+      message: 'Update file content',
+      content: btoa(updatedContent), // Encode content to Base64
+      sha: data.sha, // The current SHA of the file
+    };
+
+    // Create a URL for the API to update the file
+    const updateUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`;
+
+    // Make a PUT request to update the file
+    return fetch(updateUrl, {
+      method: 'PUT',
+      headers,
+      body: JSON.stringify(updateData),
+    });
+  })
+  .then(response => {
+    if (response.status === 200) {
+      console.log('File updated successfully.');
+    } else {
+      handleErrorResponse(response);
+    }
+  })
+  .catch(error => {
+    console.error('An error occurred:', error);
+  });};
   
